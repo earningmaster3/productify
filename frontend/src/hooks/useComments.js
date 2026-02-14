0498ecebbf6c4bfbde0d38611618c1aad0032efd
DARIUS <mketo543@gmail.com>
2026-02-14 15:01:53 +0530

    feat: add comments section with create and delete functionality; integrate comments into product page

---
+++ frontend/src/hooks/useComments.js
@@ -0,0 +1,25 @@
+import { useMutation,useQueryClient } from "@tanstack/react-query";
+import { createComment,deleteComment } from "../lib/api";
+import { use } from "react";
+
+export const useCreateComment =()=>{
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: createComment,
+        onSuccess: (_, variables) => {
+            queryClient.invalidateQueries({ queryKey: ["product", variables.productId] });
+        }   
+    })
+}
+
+export const useDeleteComment = (productId) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: deleteComment,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["product", productId] });
+    },
+  });
+};
